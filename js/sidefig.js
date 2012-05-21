778dea5eba775a50235f71f6e184d3b977925537
Joshua Shapiro <josh@shapbio.net>
2012-05-21 00:37:42 -0400

    convert sidefig.js to new align-* classes
    Note that align-* classes are assigned to the paragraph that contains the images (using knitr_jekyll_setup.R as an external source now) so these alignments are done by searching for img tags located within align-* classes. The imgs are not tagged themselves.
    
    Also, added initial code to prevent overlaps. This could probably use refinement to allow figs to shift up as well as down. Not really anxious to write the spring-based layout code or recursive positioning this will need. (It requires thought, at the very least).

--- js/sidefig.js
+++ js/sidefig.js
@@ -1,11 +1,15 @@
 //move .sidefig elements to the sidebar, keeping them near their original location
 var moveSide = function(){
-	$(".sidefig").each(function(i){
+	var min_offset = 0;
+	$(".align-sidebar img").each(function(i){
 		var oldp = $(this).parent();
-		$(this).removeClass("sidefig").appendTo("#sidebar").wrap('<div class="sideblock" />');
+		$(this).appendTo("#sidebar").wrap('<div class="sideblock" />');
 		var block = $(this).parent();
 		$(this).load(function(){
-			block.offset({top: oldp.offset().top - block.outerHeight() / 2, left: block.offset().left});
+			offset =  Math.max(min_offset, 
+							   oldp.offset().top - block.outerHeight() / 2);
+			block.offset({top: offset, left: block.offset().left});
+			min_offset = offset + block.outerHeight();
 			});
 		});
 };
---
+++ knitr/knit_jekyll_setup.R
@@ -0,0 +1,21 @@
+render_jekyll()
+opts_chunk$set(tidy=FALSE, fig.path="images/Rplot-", fig.width=5, fig.height=5)
+hook_plot_md_side <- function(x, options){
+  if (options$fig.show == 'asis' ||
+      options$fig.num == 1L || 
+      options$fig.cur == options$fig.num){
+    ## alignment class at end of list of figures: will apply to their paragraph
+    if(options$fig.align %in% c('center', 'left', 'right', 'wide', 'sidebar')){
+      div_class <- paste("{: .align-", options$fig.align, "}", sep="")
+      paste(sub("\\s+$", "", hook_plot_md(x, options)),
+            "\n", div_class, "\n", 
+            sep = ""
+      )
+    } else {
+      hook_plot_md(x, options)
+    }
+  } else {
+    hook_plot_md(x, options)
+  }
+}
+knit_hooks$set(plot = hook_plot_md_side)
