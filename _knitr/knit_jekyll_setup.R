64d8595bd1254fc359ed75630f102c120abfea06
Joshua A Shapiro <josh@shapbio.net>
2014-07-24 14:22:45 -0400

    return jekyll rendering, and change alignment code.

--- _knitr/knit_jekyll_setup.R
+++ _knitr/knit_jekyll_setup.R
@@ -1,5 +1,7 @@
 require(knitr)
 
+render_jekyll()
+
 hook_plot_md_align <- function(x, options){
   if (options$fig.show == 'asis' ||
       options$fig.num == 1L || 
@@ -8,10 +10,12 @@ hook_plot_md_align <- function(x, options){
     if(options$fig.align %in% c('center', 'left', 'right', 'wide', 'sidebar')){
       if (options$fig.align %in% c('left', 'right')){
         div_class <- paste("{: .pull-", options$fig.align, "}", sep="")
+      } else if (options$fig.align == 'center') {
+        div_class <- paste("{: .text-center}", sep="")
       } else{
         div_class <- paste("{: .", options$fig.align, "}", sep="")
       }
-      
+      options$fig.align='default' #reset so there is not double setting of the alignment
       paste(sub("\\s+$", "", hook_plot_md(x, options)),
             "\n", div_class, "\n", 
             sep = ""
